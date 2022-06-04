c5fd7ab1f10b9af1de18c5491e5e6dce2dfcba67
WayneMusungu <waynemuyera17@gmail.com>
2022-06-04 21:26:54 +0300

    Create a settings function that will enable users to update their profile image, bio and location

--- insta/views.py
+++ insta/views.py
@@ -11,6 +11,36 @@ from insta.models import Profile
 def index(request):
     return render(request, 'index.html')
 
+
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        
+        return redirect('settings')
+    return render(request, 'settings.html', {'user_profile': user_profile})
+
+
 def signup(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -31,11 +61,13 @@ def signup(request):
                 user.save() 
                 
                 # Log in the user and redirect them to the settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
                 # Create a profile object for the new user 
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id )
                 new_profile.save()
-                return redirect('signup')
+                return redirect('setting')
         else:
             messages.info(request, 'Your Password Do Not Match')
             return redirect('signup')
@@ -61,6 +93,7 @@ def signin(request):
         
         return render(request, 'signin.html')
 
+@login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
     return redirect('signin')
