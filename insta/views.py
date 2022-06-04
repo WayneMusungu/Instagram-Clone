c6a80c9d4ee19ca33e2c6cb634cec3c058b3a65f
WayneMusungu <waynemuyera17@gmail.com>
2022-06-04 22:10:12 +0300

    Create a user object that will display the profile photo of a user

--- insta/views.py
+++ insta/views.py
@@ -9,7 +9,15 @@ from insta.models import Profile
 # Create your views here.
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    return render(request, 'index.html', {'user_profile':user_profile})
+
+@login_required(login_url='signin')
+def upload(request):
+    return HttpResponse('<h1>Upload View</h1>')
+# render(request, 'upload.html')
+
 
 
 @login_required(login_url='signin')
