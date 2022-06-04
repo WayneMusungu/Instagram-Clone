d0a973dd928933ec98b8f8d33d45e51b69d15299
WayneMusungu <waynemuyera17@gmail.com>
2022-06-04 21:48:45 +0300

    Create a post model an reister it to admin

--- insta/models.py
+++ insta/models.py
@@ -1,6 +1,12 @@
+from distutils.command.upload import upload
+from email.mime import image
 from email.policy import default
 from django.db import models
 from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
+
+
 
 User = get_user_model()
 
@@ -14,3 +20,16 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.user.username
+    
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=120)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now)
+    no_of_likes =models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.user
+        
+    
