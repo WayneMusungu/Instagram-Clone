946d1bb3843be7aa1102ed889b10e079fd471c3d
WayneMusungu <waynemuyera17@gmail.com>
2022-06-05 07:57:53 +0300

    Create a like post function that will enable registered users to like and dislike a post

--- insta/urls.py
+++ insta/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
    path('signup/', views.signup, name='signup'),
    path('signin/', views.signin, name='signin'),
    path('upload', views.upload, name='upload'),
-   path('like-post', views.like_post, name='like-post'),
+   path('like-post/', views.like_post, name='like-post'),
    path('logout/', views.logout, name='logout'),
   
 ]
